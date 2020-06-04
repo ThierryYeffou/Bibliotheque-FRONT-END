62cce83e4e95725dddc4a2bfda08a3a69743f601
Thierry Yeffou <yeffouthierry@gmail.com>
2020-06-04 16:15:13 +0200

    components homepage

---
+++ bibliotheque/src/app/components/homepage/homepage.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  template: './homepage.components.html',
+  styles: [
+  ]
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
