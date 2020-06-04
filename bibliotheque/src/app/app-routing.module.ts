1ba5cec5f98bc739b780b806c8d84494174f2254
Benoit Técher <techer.benoit2113@gmail.com>
2020-06-04 16:54:33 +0200

    mise à jour

--- bibliotheque/src/app/app-routing.module.ts
+++ bibliotheque/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConnexionComponent } from './components/connexion/connexion.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{path:'connexion', component:ConnexionComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
