cfc0e543598dbc26ac70fb4a2fd947e210332925
souleymane Diallo <dialau85@yahoo.fr>
2020-06-04 23:38:40 +0200

    modification routing

--- bibliotheque/src/app/app-routing.module.ts
+++ bibliotheque/src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 
-const routes: Routes = [{path:'connexion', component:ConnexionComponent}];
+const routes: Routes = [
+  {path:'connexion', component:ConnexionComponent},
+  {path: 'homepage', component: HomepageComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'footer', component: FooterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bibliotheque/src/app/components/menu/menu.component.ts
+++ bibliotheque/src/app/components/menu/menu.component.ts
@@ -2,13 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
-  template: `
-    <p>
-      menu works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './menu.component.html',
+  styleUrls: []
 })
 export class MenuComponent implements OnInit {
 
