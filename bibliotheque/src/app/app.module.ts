4b4de06efaff841409c3a942059cd7b59b57b43d
kdoranco <rk_doranco-cda@gmx.com>
2020-06-04 17:13:54 +0200

    commit de conflit

--- bibliotheque/src/app/app.module.ts
+++ bibliotheque/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
