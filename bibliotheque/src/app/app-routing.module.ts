d42f5296de7c8e362b9f26bdc0accb7f468546b3
souleymane Diallo <dialau85@yahoo.fr>
2020-06-05 08:52:32 +0200

    ajout lien menu

--- bibliotheque/src/app/app-routing.module.ts
+++ bibliotheque/src/app/app-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { MenuComponent } from './components/menu/menu.component';
+
 
 
 const routes: Routes = [
   {path:'connexion', component:ConnexionComponent},
   {path: 'homepage', component: HomepageComponent},
-  {path: 'menu', component: MenuComponent},
-  {path: 'footer', component: FooterComponent}
+  
 ];
 
 @NgModule({
