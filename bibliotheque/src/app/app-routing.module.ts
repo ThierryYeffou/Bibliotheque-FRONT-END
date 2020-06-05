072ea0a5e648629549e3d60b99c8aaf32dfb9be6
Thierry Yeffou <yeffouthierry@gmail.com>
2020-06-05 09:17:43 +0200

    add component homepage create borrowing

--- bibliotheque/src/app/app-routing.module.ts
+++ bibliotheque/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConnexionComponent } from './components/connexion/connexion.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { CreateBorrowingComponent } from './components/create-borrowing/create-borrowing.component';
+import { CloseBorrowingComponent } from './components/close-borrowing/close-borrowing.component';
 
-
-const routes: Routes = [{path:'connexion', component:ConnexionComponent}];
+const routes: Routes = [
+  {path:'connexion', component:ConnexionComponent},
+  {path:'homepage', component:HomepageComponent},
+  {path:'create-borrowing', component:CreateBorrowingComponent},
+  {path:'close-borrowing', component:CloseBorrowingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bibliotheque/src/app/app.module.ts
+++ bibliotheque/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CreateBorrowingComponent } from './components/create-borrowing/create-borrowing.component';
+import { CloseBorrowingComponent } from './components/close-borrowing/close-borrowing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    CreateBorrowingComponent,
+    CloseBorrowingComponent
   ],
   imports: [
     BrowserModule,
